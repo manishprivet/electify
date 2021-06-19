6adfaa26f9cafefaacda828700abcf75115a472b
Manish Kumar <manishprivet@protonmail.com>
2021-06-19 15:21:40 +0530

    feat(page): added buymeacoffee button
    Signed-off-by: Manish Kumar <manishprivet@protonmail.com>

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -50,6 +50,18 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          <script
+            data-name="BMC-Widget"
+            data-cfasync="false"
+            src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
+            data-id="manishprivet"
+            data-description="Support me on Buy me a coffee!"
+            data-message=""
+            data-color="#FF813F"
+            data-position="Right"
+            data-x_margin="18"
+            data-y_margin="18"
+          />
         </body>
       </Html>
     );
